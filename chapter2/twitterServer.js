b5e7ca4fc0b0b10ee5aac8fe3ada53a45c931c2d
kevinchau321 <kevinchau321@gmail.com>
2016-11-15 20:51:13 -0800

    added bodyParser module

--- chapter2/twitterServer.js
+++ chapter2/twitterServer.js
@@ -1,9 +1,27 @@
 var express = require('express')
+ var bodyParser = require('body-parser');
 
-var app = express.createServer()
+// createServer() is deprecated
+// var app = express.createServer()
+var app = express()
+
+app.listen(8000)
+
+var tweets = []
 
 app.get('/', function(req,res){
   res.send('Welcome to Node Twitter')
 })
 
-app.listen(8000)
+app.post('/send', bodyParser(), function(req,res) {
+  if (req.body && req.body.tweet) {
+    tweets.push(req.body.tweet)
+    res.send({status:"ok", message:"Tweet received"})
+  } else {
+    res.send({status:"nok", message:"No tweet received"})
+  }
+})
+
+app.get('/tweets', function(req,res) {
+  res.send(tweets)
+})
