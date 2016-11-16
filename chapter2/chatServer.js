93ff272620dcbb51ea094914c57f790ac5f760ae
kevinchau321 <kevinchau321@gmail.com>
2016-11-15 20:02:23 -0800

    logging quit and errors

--- chapter2/chatServer.js
+++ chapter2/chatServer.js
@@ -12,13 +12,32 @@ chatServer.on('connection', function(client) {
   client.on('data', function(data){
     broadcast(data,client)
   })
+
+  client.on('end', function() {
+    console.log(client.name + ' quit')
+    clientList.splice(clientList.indexOf(client),1)
+  })
+
+  client.on('error', function() {
+    console.log(e)
+  })
 })
 
 function broadcast(message,client) {
+  var cleanup = []
   for(var i = 0; i < clientList.length; i += 1){
     if(client !== clientList[i]) {
-      clientList[i].write(client.name + " says " + message)
+
+      if(clientList[i]){
+        clientList[i].write(client.name + " says " + message)
+      } else {
+        cleanup.push(clientList[i])
+        clientList[i].destroy()
+      }
     }
+  }
+  for(i=0;i<cleanup.length;i+=1){
+    clientList.splice(clientList.indexOf(cleanup[i]),1)
   }
 }
 
